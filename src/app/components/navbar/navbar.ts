0a53b1cc430b2257f827efff9628943d517595e1
anjali20006 <anjalikumari20066371@gmail.com>
2026-06-07 13:13:41 +0530

    fix: restore missing 10-question dosha quiz and implement results view (#109)
    Co-authored-by: UDISHA NARAYAN <udishajvm@gmail.com>

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
